98fa06b96c31fbd52d99432e163f01dab215dd24
minmin9324 <minmin93240@gmail.com>
2025-03-07 02:19:40 +0900

    소유자 기반 데이터 분리

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,13 @@ function App() {
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
 
   return (
     <main>
       <button onClick={signOut}>Sign out</button>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      {/* <h1>My todos</h1> */}
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
