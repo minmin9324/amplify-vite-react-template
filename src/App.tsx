1a71fa0886ea8eb288df13ce82e829c4f77ba8b4
minmin9324 <minmin93240@gmail.com>
2025-03-07 01:42:36 +0900

    add authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
 const client = generateClient<Schema>();
 
@@ -16,14 +17,21 @@ function App() {
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
+  const { signOut } = useAuthenticator();
 
   return (
     <main>
+      <button onClick={signOut}>Sign out</button>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li
+            onClick={() => client.models.Todo.delete({ id: todo.id })}
+            key={todo.id}
+          >
+            {todo.content}
+          </li>
         ))}
       </ul>
       <div>
